95001bc5aa86a04b239d2ce8eacb523576e510b4
Jasper <J.M.vanSelm@student.tudelft.nl>
2023-05-25 20:15:55 +0200

    Add name engineer 3 to name.py

--- name.py
+++ name.py
@@ -1,12 +1,15 @@
 # Print out all the names of all the engineers
 
+
+
 def print_names(names):
     print(f'We are the new epoch engineers: {names}')
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_names('baasje1')
+    names = ["Jasper"]
+    print_names(names)
 
 # klein testje
 
