eccee4dddeb699d6f2234c7978b81e0feeffb2c9
tolgakopar <cahittolgakopar@gmail.com>
2023-05-25 20:56:47 +0200

    Revert "fix for bug 11"
    This reverts commit 4dfcdb9afd755fbe2de29876640946dfa8ab6a4d.

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ average = engineer.average_project_score
 print("Average project score:", average)
 
 # Bug 11: Trying to add a string instead of a project score (integer)
-engineer.add_project_score(100)
+engineer.add_project_score("Excellent")
 
 print("Project scores after adding a new score:", engineer.projects)
 
