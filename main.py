546a735f53df2551636b8878f4b736519abfd43c
Adrian <kuiper2000@hotmail.com>
2023-05-25 16:49:40 +0200

    main.py names for issues

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+# Print out all the names of all the engineers
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hiha, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def print_names(names):
+    print(f'We are the new epoch engineers: {names}')
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_names('baasje1')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
